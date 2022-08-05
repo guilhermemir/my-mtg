69446dac0b824fdd248d7f143c1f4a94cf16c174
Guilherme Mir <guilherme.mir@gmail.com>
2022-08-05 00:49:34 -0300

    search update

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,11 +6,23 @@ class Card < ApplicationRecord
   end
 
   def image_url
-    scryfall["image_uris"]["large"]
+    scryfall.dig("image_uris", "large").presence ||
+      scryfall.dig("image_uris", "normal").presence ||
+      scryfall.dig("image_uris", "small").presence ||
+      scryfall.dig("card_faces", 0, "image_uris", "large").presence ||
+      scryfall.dig("card_faces", 0, "image_uris", "normal").presence ||
+      scryfall.dig("card_faces", 0, "image_uris", "small").presence ||
+      "https://via.placeholder.com/672x936"
   end
 
+  # Returns the web URL for the card (to visit in the browser).
   def scryfall_url
     scryfall["scryfall_uri"]
+  end
+
+  # Returns the API URL for the card (JSON).
+  def scryfall_api_url
+    "https://api.scryfall.com/cards/#{scryfall_id}"
   end
 
   # Returns the *first* card in Scryfall with given name.
@@ -25,13 +37,15 @@ class Card < ApplicationRecord
   # Returns a *list of all* cards in Scryfall with given name.
   def self.scryfall_find_all_by_name(name)
     encoded_name = ERB::Util.url_encode(name)
-    r = HTTP.get("https://api.scryfall.com/cards/search?unique=prints&order=released&q=#{encoded_name}")
+    r = HTTP.get("https://api.scryfall.com/cards/search?include_multilingual=false&unique=prints&include_extras=true&include_variations=false&pretty=false&dir=asc&order=released&q=#{encoded_name}")
     search_result = JSON.parse(r.to_s)
 
-    search_result["data"].map do |c|
+    cards = search_result["data"].map do |c|
+      name = c["printed_name"].presence || c["name"]
+
       {
         id: c["id"],
-        name: "#{c["name"]} (#{c["set_name"]}, ##{c["collector_number"]})",
+        name: "#{name} (#{c["set_name"]}, ##{c["collector_number"]}})",
       }
     end
   end
@@ -41,7 +55,7 @@ class Card < ApplicationRecord
     return @scryfall if @scryfall.present?
 
     @scryfall = Rails.cache.fetch("scryfall/cards/#{scryfall_id}", expires_in: 1.day) do
-      r = HTTP.get("https://api.scryfall.com/cards/#{scryfall_id}")
+      r = HTTP.get(scryfall_api_url)
       JSON.parse(r.to_s)
     end
 
